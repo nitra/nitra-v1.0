c8d2d4d49853b8f23ba1fb2f73487072722e7340
Korobeynik <korobeynik77@gmail.com>
2018-06-06 17:36:43 +0300

    Fixes nitra

--- themes/introduction/static/js/common.js
+++ themes/introduction/static/js/common.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+
+    //var url = $(document).location.hash;
+
+    $('.anchor').waypoint(function() {
+        var location = $(location).attr('href');
+        var elem = ($(this.element).attr('id'));
+       window.location.hash = elem;
+
+
+    });
+
     $(".ninja-btn").click(function() {
         $(this).toggleClass("active");
         $("#side-nav").toggleClass("active");
