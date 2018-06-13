870bdfb76e535d6846b660f89819cf47bd459899
Korobeynik <korobeynik77@gmail.com>
2018-06-13 11:24:08 +0300

    Changed main page

--- themes/introduction/static/js/common.js
+++ themes/introduction/static/js/common.js
@@ -11,21 +11,21 @@ $(document).ready(function() {
 
     });
 
-    $(".ninja-btn").click(function() {
-        $(this).toggleClass("active");
-        $("#side-nav").toggleClass("active");
-        $("#content").toggleClass("active");
-    });
+    // $(".ninja-btn").click(function() {
+    //     $(this).toggleClass("active");
+    //     $("#side-nav").toggleClass("active");
+    //     $("#content").toggleClass("active");
+    // });
 
-    $("#content").click(function() {
-        if ($(this).hasClass("active")) {
-            $(".ninja-btn").toggleClass("active");
-            $(this).toggleClass("active");
-            $("#side-nav").toggleClass("active");
-        } else {
-            console.log('.');
-        }
-    });
+    // $("#content").click(function() {
+    //     if ($(this).hasClass("active")) {
+    //         $(".ninja-btn").toggleClass("active");
+    //         $(this).toggleClass("active");
+    //         $("#side-nav").toggleClass("active");
+    //     } else {
+    //         console.log('.');
+    //     }
+    // });
 
     $('.button-wrap .btn-primary').addClass('btn-lg');
 
